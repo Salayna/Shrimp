d775e8c7234efe1e377a70470b0759c1f7beef7a
Salayna <salaynadoukoure@gmail.com>
2021-01-16 22:47:54 +0100

    feat: created prompts for the project name and the project language

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Salayna/create-project-cli
 
 go 1.15
 
-require rsc.io/getopt v0.0.0-20170811000552-20be20937449
+require (
+	github.com/manifoldco/promptui v0.8.0
+	rsc.io/getopt v0.0.0-20170811000552-20be20937449
+)
--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -2,24 +2,26 @@ package cli
 
 import (
 	"flag"
+	"fmt"
+	"log"
+	"os"
 
-	filesystem "github.com/Salayna/create-project-cli/internal/arborescence"
+	"github.com/manifoldco/promptui"
 	"rsc.io/getopt"
 )
 
 var name string
 var file string
 var language string
-var verbose bool
-var version bool
+var v bool
 
 //Init initialize the cli flags
 func Init() {
 	flag.StringVar(&name, "name", "", "Set the name of the project")
 	flag.StringVar(&language, "language", "", "get the language")
-	flag.BoolVar(&verbose, "verbose", false, "show verbose output if there are multiple arguments")
-	flag.BoolVar(&version, "version", false, "Show current cli version")
-	flag.StringVar(&file, "file", "./config.yaml", "create a project from a yaml file")
+	flag.BoolVar(&v, "verbose", false, "show verbose output if there are multiple arguments")
+	flag.BoolVar(&v, "version", false, "Show current cli version when only -v is called")
+	flag.StringVar(&file, "file", "", "create a project from a yaml file")
 	//flag.StringVar(&help, "help", flag.PrintDefaults(), "Display help")
 	getopt.Aliases(
 		"n", "name",
@@ -33,9 +35,62 @@ func Init() {
 	getopt.Parse()
 }
 
+//Version show the version of the current cli
+func Version() {
+	if v == true && len(os.Args) == 2 {
+		fmt.Println("V 1.0.0")
+	}
+}
+
+func promptName() string {
+	namePrompt := promptui.Prompt{
+		Label: "Project Name(or directory)",
+	}
+	promptedName, err := namePrompt.Run()
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+	return promptedName
+}
+
+func promptLanguage() string {
+	langPrompt := promptui.Select{
+		Label: "Language",
+		Items: []string{"Javascript", "Typescript", "Golang", "C++"},
+	}
+
+	_, promptLanguage, langErr := langPrompt.Run()
+	if langErr != nil {
+		log.Fatal(langErr)
+		os.Exit(1)
+	}
+	return promptLanguage
+}
+
+//Interactive handles Interactive mode
+func Interactive() {
+
+	if name == "" {
+		name = promptName()
+	}
+	if language == "" {
+		language = promptLanguage()
+	}
+	fmt.Println("Project name is", name)
+	fmt.Println("Type", language)
+}
+
 //Cli is the cli function which will handle all the cli calls
 func Cli() {
 	Init()
-	args := []string{"init", "-y"}
-	filesystem.ExecuteCommand("npm", args)
+	fmt.Println(file)
+	if (((name) == "") || (language == "")) && (v == false) && (file == "") {
+		Interactive()
+	} else if file != "" {
+		if (name != "") || (language != "") {
+			log.Fatal("You can't pass other arguments with -f or --file")
+		}
+	}
+	Version()
 }
